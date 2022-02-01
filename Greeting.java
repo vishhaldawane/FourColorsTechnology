2972fd988eef080a9fcea11d75b3b04d971ba906
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-02-01 14:56:05 +0530

    NOTES FOR THE CONSTRUCTOR

--- Greeting.java
+++ Greeting.java
@@ -52,13 +52,8 @@ class Welcome { // Welcome.java
 	 
  }
  
- class SavingsAccount { 
-	 void withdraw() {
-		 System.out.println("withdrawing.... amount...from savings account..");
-	 }
-	 
-	 
- }
+
+
  
  
  /*
@@ -138,11 +133,185 @@ class Welcome { // Welcome.java
     ->			->				->			->
     	
     
-   DATA TYPEs
+   								DATA TYPEs
+   									|
+   				----------------------------------------
+   				|								|
+   				Primitive					Secondary
+   				|									|
+   	------------------------------			----------------------------
+   	|		|		|			|			|java.lang <-- default package 
+ Integral Floating Character  Boolean		Object <-- root class of the java hierarchy
+ |											|
+ byte  1								--------------------------
+ short 2								|		|	|			|
+ int   4							String	 Joker	Greeting	Welcome
+ long  8
+ 
+ windows		unix
+ tc	 / VC++		tc
+ |		|		|
+ int	int		int
+ 2		4		4
+ 
+ difference in size would impact on the range it holds
+ 
+ size of all types is fixed across all JVMs
+ 
+ 
+ Types of coding			Standard Naming Convention
+ |							|
+ class						ClassName		DriverManager  SavingsAccount FixedDepositAccount - PascalCase
+ 
+ variable					variableName	rollNumber,  employeeNumber, socialSecurityNumber - cameCase	
+ method						methodName		withdrawCash(), setLookAndFeel(), transferFunds()
+
+											godisnowhere()  godIsNoWhere(), godIsNowHere()
+
+ constant					CONSTANT		PI  MAX  MIN   SPEED_OF_LIGHT  GRAVITY	
+ package					main.subpack	com.fourcolorurs,  java.swing.JFrame
+ 
+ 
+ 
+ 
+ 
    
    
     
+    byte = 1 byte
     
+    
+    	1 byte = 8 bits
+    	
+    	
+    	1 bit =   0   on					2
+    			  1	  off
+    			  
+    	2 bits	=	 0 0					4
+    				 0 1
+    				 1 0
+    				 1 1
+    				 
+    	3 bits = 	0	0	0		0		8
+    				0	0	1		1
+    				0	1	0		2
+    				0	1	1		3
+    				1	0	0		4
+    				1	0	1		5
+    				1	1	0		6
+    				1	1	1		7
+    				
+    	4 bits = ..							16
+    	5									32
+    	6									64
+    	7									128
+    	
+    	
+    	
+    	<---------------------------------256-------------------------->
+    	-128	-126	-3  -2   -1			0		1 	2	3	...  127
+    	
+    	
+    	
+    	
+    	
+    	
+    	Classes				vs				Objects
+    	|									|
+    	|									|
+    	design of the "object"			instance of a "class"
+    	logical entity					realtime/physical entity
+    	eg. blueprint of a house		actual house
+    	its just an english code		realtime entity in the memory
+    	 
+    	
+    	
+    	class SavingsAccount			SavingsAccount savObj1 = new SavingsAccount();
+    	{								SavingsAccount savObj2 = new SavingsAccount();
+    			data members			SavingsAccount savObj3 = new SavingsAccount();
+    			+
+    			member functions/
+    			methods
+    	}
+    	
+    	
+    	
+    Constructor
+    ------------
+    
+    	
+    		a constructor is a function which has the same name as of the 
+    		enclosing class
+    		
+    		
+    		class Person
+    		{
+    				Person(int x, int j) <--- same name as of the class
+    				{
+    				
+    				}
+    				Person(int i) {
+    				}
+    				Person(String str) {
+    				}
+    				
+    		}
+    		
+    		- it does not have return type
+    		- but it can take parameters
+    		
+    		- hence it can be overloaded
+    		
+    		- it is automatically executed during the object creation process
+    		
+    		Person thePerson = new 	Person();
+    		|			 |		 |		|
+    		className refName allocator constructor
+    		
+    		if the constructor is failed to execute, then
+    		the new keyword would fail the allocation too.
+    		
+    		
+    		
+    		- INITIALIZE THE state of the object
+    		
+    		- every class has an "implicit constructor" - provided by JPL
+    		
+    				- it is also known as the "default constructor"
+    				
+    				
+    				
+    		class Student
+    		{
+    				 <-- there is a default constructor
+    				
+    		}
+    		
+    		class Employee
+    		{
+    				Employee() // explicit no-arg constructor
+    				{
+    				
+    				}
+    		}
+    		
+    		at a time you can have either the implicit or the explicit
+    		but no
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    		
+    	
+    	
+    	
+    	
+    	
+    				
+    				
     
     
     
