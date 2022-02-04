1f816e4c621f6a033ea0ba31cf360693b2b4be1c
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-02-04 10:56:00 +0530

    Add files via upload

--- ChessTest.java
+++ ChessTest.java
@@ -1,12 +1,15 @@
 
 public class ChessTest {
 	public static void main(String[] args) {
+		
+		
 
-		Chess myChess = new Chess(30, 31, true, 2);
+		Chess myChess = new GraphicalChess(30, 31, true, 2,400,400);//it has the business logic of the chess
+		//, it may not have graphical logic to show on the screen
 
 		System.out.println("myChess : " + myChess);
 
-
+		myChess.moveBishop();
 
 	}
 }
@@ -23,7 +26,13 @@ class LoudSpeaker extends Speaker {
 	}
 }
 
-class Chess {
+		//Knowledge -> applied -> Wisdom
+
+		//The word/spelling "Water" is not the actual WATER  - 
+
+		// experience of the WATER
+
+class Chess { // Sumit is the developer of this class - 2 months time to develop
 	private final int numberOfPlayers = 2; // fixed
 	private final int numberOfSquares = 64; // fixed
 
@@ -51,48 +60,7 @@ class Chess {
 		this.winner = winner;
 	}
 
-	public int getNumberOfPlayers() {
-		return numberOfPlayers;
-	}
-
 	
-	public int getNumberOfSquares() {
-		return numberOfSquares;
-	}
-
-
-	public int getNumberOfMovesByPlayer1() {
-		return numberOfMovesByPlayer1;
-	}
-
-	public void setNumberOfMovesByPlayer1(int numberOfMovesByPlayer1) {
-		this.numberOfMovesByPlayer1 = numberOfMovesByPlayer1;
-	}
-
-	public int getNumberOfMovesByPlayer2() {
-		return numberOfMovesByPlayer2;
-	}
-
-	public void setNumberOfMovesByPlayer2(int numberOfMovesByPlayer2) {
-		this.numberOfMovesByPlayer2 = numberOfMovesByPlayer2;
-	}
-
-	public boolean isCheckMate() {
-		return isCheckMate;
-	}
-
-	public void setCheckMate(boolean isCheckMate) {
-		this.isCheckMate = isCheckMate;
-	}
-
-	public int getWinner() {
-		return winner;
-	}
-
-	public void setWinner(int winner) {
-		this.winner = winner;
-	}
-
 	@Override
 	public String toString() {
 
@@ -100,5 +68,101 @@ class Chess {
 				+ ", numberOfMovesByPlayer1=" + numberOfMovesByPlayer1 + ", numberOfMovesByPlayer2="
 				+ numberOfMovesByPlayer2 + ", isCheckMate=" + isCheckMate + ", winner=" + winner + "]";
 	}
+	
+	/*
+	 					      player1
+1	 	 	Rook Knight Bishop King Queen Bishop Knight Rook
+2	 	 	Pawn Pawn	Pawn   Pawn Pawn  Pawn   Pawn   Pawn <==white colors
+3	 	 	
+4
+5
+6	 	 	
+7	 	 	Pawn Pawn	Pawn   Pawn Pawn  Pawn   Pawn   Pawn
+8	 	 	Rook Knight Bishop King Queen Bishop Knight Rook <== black colors
+	 	 					  player2
+	 	 					  
+	 	 					  
+	 	 					  
+	 	 	Ele  Horse  Camel ..   	 King		Vazeer				Pawn
+	 	 	|	   |     |				|		 |					 |
+	  vert/hori   L-Shape  diagonal  1step		vert/hori/diagonal	forward/diagonal		  
+	  N-steps    one-step  N-steps	 anywhere	n-steps				1step	
+	 bi-direct  bi-direct bi-direct			    bi-directional      unidirectional
+	 */
+	
+	final void moveBishop() { //final version of the method, can never be overridden
+		System.out.println("Math Moving...bishop diagonal way...N-steps...bi-directionally..");
+		//math logic can be calculated here...
+	}
+	void moveKnight() { 
+		System.out.println("Math Moving...Knight L-Shape way...1-step...bi-directionally..");
+		//math logic can be calculated here...
+	}
+	void moveRook() { 
+		System.out.println("Math Moving...Rook horizonal/vertical way...N-steps...bi-directionally..");
+		//math logic can be calculated here...
+	}
+	void moveKing() { 
+		System.out.println("Math Moving...King 1-steps...anywhere..");
+		//math logic can be calculated here...
+	}
+	void moveQueen() { 
+		System.out.println("Math Moving...Queen horizontal/vertical/diagonal way...N-steps...bi-directionally..");
+		//math logic can be calculated here...
+	}
+	void movePawn() { 
+		System.out.println("Math Moving...Pawn ahead/diagonal way...1-steps...uni-directionally..");
+		//math logic can be calculated here...
+	}
+	
 
 }
+
+//this is the final version of the class, can never be extended
+
+final class GraphicalChess extends Chess // developed by Gayatri
+{
+	int width;
+	int height;
+	
+	
+	public GraphicalChess(int numberOfMovesByPlayer1, int numberOfMovesByPlayer2, boolean isCheckMate, int winner,
+			int width, int height) {
+		super(numberOfMovesByPlayer1, numberOfMovesByPlayer2, isCheckMate, winner);
+		this.width = width;
+		this.height = height;
+	}
+
+	void moveGUIBishop() {  //real overriding - when u honor the code of the super class
+			//plus you develop/extend/refine/polish your code too.....
+		
+		super.moveBishop();//borrow the math logic 
+		System.out.println("Graphical Moving...bishop anywhere...N-steps...any-directionally..");
+		//math logic can be calculated here...
+	}
+}
+
+class GraphicalAnimatedChess extends GraphicalChess { //Vishhal
+	
+	int animationLevel; //2D 3D 4D 5D 9D
+	
+	public GraphicalAnimatedChess(int numberOfMovesByPlayer1, int numberOfMovesByPlayer2, boolean isCheckMate,
+			int winner, int width, int height, int animationLevel) {
+		super(numberOfMovesByPlayer1, numberOfMovesByPlayer2, isCheckMate, winner, width, height);
+		this.animationLevel = animationLevel;
+	}
+
+
+	void moveGUIBishop() {  //real overriding - when u honor the code of the super class
+		//plus you develop/extend/refine/polish your code too.....
+	
+		super.moveBishop();//borrow the math logic 
+		System.out.println("Graphical Animated Moving...bishop L shape V Shape U Shape...N-steps...any-directionally..");
+		//math logic can be calculated here...
+	}
+}
+
+
+
+
+
