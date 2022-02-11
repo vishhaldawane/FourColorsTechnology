2d4e40ae5a9bd129dec38d78ae20923550d69a97
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-02-11 14:49:51 +0530

    ORACLE STEPS

--- SelectTest.java
+++ SelectTest.java
@@ -71,3 +71,48 @@ public class SelectTest {
 	}
 
 }
+/*
+
+I. connect to oralce db interface screen 
+
+c:\app\UR_USER_NAME\dbhomeXE\bin>  sqlplus <---application file to be doubliclicked
+
+c:\ ...it would ask to enter orale username > system
+..oracle user's password : ....enter password here
+
+
+II. create table, insert records, view records, commit them
+
+SQL> create table dept_fc
+  (
+    deptno number primary key,
+    deptname varchar2(20),
+    deptloc  varchar2(20)
+  );
+
+  insert into dept_fc values (10,'IT','Mumbai');
+  insert into dept_fc values (20,'Sales','Delhi');
+  insert into dept_fc values (30,'QMS','Pune');
+  insert into dept_fc values (40,'Testing','Chennai');
+
+  SELECT * FROM DEPT_FC;
+
+SQL> commit;
+
+III.
+    C:\app\DELL\product\18.0.0\dbhomeXE\network\admin
+​    tnsname.ora to be review
+​    find out to the host name
+
+IV. services.msc <-- from windows search button
+​
+    locate the ---> oracleoradb18homelistener
+	​right click on it ->    to be started -> start 
+​
+V. make these 2 changes in SelectTest.java
+    DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+​    Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@192.168.2.3:1521:xe","system","system");
+
+
+
+*/
