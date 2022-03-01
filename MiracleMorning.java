31cc72275cf4514dd9e0959f6b53e0af95490788
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-03-01 18:14:11 +0530

    Miracle Morning

---
+++ MiracleMorning.java
@@ -0,0 +1,42 @@
+/*
+
+
+1 st 
+group - 5am - 6am = 21 days cycle = neural network 
+
+
+
+MIRACLE MORNING - HAL ELROD
+		The Height of your SUCCESS [ health + wealth + relationships + mental peace ]
+				|
+				is always greater than the 
+							|
+							Height of your  PERSONAL DEVELOPMENT
+												|		dhyanam - tool
+							10 			----------------------------------------------------
+							9 			|		|		|		|		|		|		|
+							8			d		h		y		a		n		a		m
+							7			|10		|5		|15		|10		|5		|15		|5 = 1 hour 
+							..			|		|		|		|		|  atma-vikas	main priorities
+										|		|		|		|		|		BOOK 	urgen+important
+							6	detoxification  |	  yoga/		atma	navigated
+							5			handwritten	 exercise  sadhana  visualization
+							4			affirmations			*meditation   
+							3			/goal + sign			theta - alpha 
+							2<--  success
+							1<-- personal development
+							0
+						------------
+				
+				alpha
+			5am, 6am, 7am   8 9 10 11 12 13 14   to    12 midnight
+
+detoxification - [500ml water in warm copper vessel] non-stop - 2 glasses - sit in utktasan
+
+
+				western toilet 
+				
+				
+
+
+*/
